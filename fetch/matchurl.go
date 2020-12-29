ad686399dafc0a2131d19b53e6d102d069815adc
Soumik Sarkar <soumiksarkar.3120@gmail.com>
2020-12-30 01:53:15 +0530

    Ignore spoilered URLs in messages

--- fetch/matchurl.go
+++ fetch/matchurl.go
@@ -4,6 +4,7 @@ import (
 	"net/url"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 var (
@@ -18,6 +19,7 @@ var (
 // ParseBlogURLs parses Codeforces blog URLS from the given string.
 func ParseBlogURLs(s string) []BlogURLMatch {
 	var matches []BlogURLMatch
+	s = removeSpoilers(s)
 	for _, idx := range blogURLRe.FindAllStringSubmatchIndex(s, -1) {
 		if checkEmbedsSuppressed(s, idx[0], idx[1]) {
 			continue
@@ -41,6 +43,7 @@ func ParseBlogURLs(s string) []BlogURLMatch {
 
 // ParseProblemURLs parses Codeforces problem URLS from the given string.
 func ParseProblemURLs(s string) []ProblemURLMatch {
+	s = removeSpoilers(s)
 	var matches []ProblemURLMatch
 	for _, idx := range problemURLRe.FindAllStringSubmatchIndex(s, -1) {
 		if checkEmbedsSuppressed(s, idx[0], idx[1]) {
@@ -60,6 +63,7 @@ func ParseProblemURLs(s string) []ProblemURLMatch {
 
 // ParseSubmissionURLs parses Codeforces submission URLs from the given string.
 func ParseSubmissionURLs(s string) []SubmissionURLMatch {
+	s = removeSpoilers(s)
 	var matches []SubmissionURLMatch
 	for _, idx := range submissionURLRe.FindAllStringSubmatchIndex(s, -1) {
 		if checkEmbedsSuppressed(s, idx[0], idx[1]) {
@@ -87,6 +91,28 @@ func ParseSubmissionURLs(s string) []SubmissionURLMatch {
 		matches = append(matches, match)
 	}
 	return matches
+}
+
+// Removes spoilered parts of the input string (parts surrounded by ||).
+func removeSpoilers(s string) string {
+	marker := "||"
+	var result strings.Builder
+	for {
+		var i int
+		if i = strings.Index(s, marker); i == -1 {
+			result.WriteString(s)
+			break
+		}
+		result.WriteString(s[:i])
+		s = s[i+len(marker):]
+		if i = strings.Index(s, marker); i == -1 {
+			result.WriteString(marker)
+			result.WriteString(s)
+			break
+		}
+		s = s[i+len(marker):]
+	}
+	return result.String()
 }
 
 // Checks whether the given substring is surrounded by <>. Used to check if a link embed is
